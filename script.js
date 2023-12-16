ae251f45794a622ff2ce3157d9ed33533f58961e
GabrielAsis <asis_gabriel24@yahoo.com>
2023-12-16 14:15:57 +0400

    Nav, Slogan, and Trailer

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 let video = document.querySelector(".video") 
 let hover_text = document.querySelector(".hover_text")
 
-hover_text.addEventListener("mouseover", function (e) { 
-    document.querySelector(".hover_text").style.visibility='hidden';
+hover_text.addEventListener("mouseenter", function (e) { 
+    document.querySelector(".hover_text p").style.visibility='hidden';
     video.play(); 
 }) 
 
-hover_text.addEventListener("mouseout", function (e) { 
-    document.querySelector(".hover_text").style.visibility='visible';
+hover_text.addEventListener("mouseleave", function (e) { 
+    document.querySelector(".hover_text  p").style.visibility='visible';
     video.pause(); 
 }) 
