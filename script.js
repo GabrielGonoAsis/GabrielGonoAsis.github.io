e28b214ee8ce0c4ed263409a654706183973aae1
GabrielAsis <asis_gabriel24@yahoo.com>
2023-12-16 16:44:01 +0400

    Overview Section

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 let video = document.querySelector(".video") 
+document.querySelector(".video").style.transition = "0.3s ease";
 let hover_text = document.querySelector(".hover_text")
+document.querySelector(".hover_text p").style.transition = "0.3s ease";
 
 hover_text.addEventListener("mouseenter", function (e) { 
-    document.querySelector(".hover_text p").style.visibility='hidden';
+    document.querySelector(".hover_text p").style.opacity='0';
+    document.querySelector(".video") .style.filter='brightness(100%)';
     video.play(); 
 }) 
 
 hover_text.addEventListener("mouseleave", function (e) { 
-    document.querySelector(".hover_text  p").style.visibility='visible';
+    document.querySelector(".hover_text  p").style.opacity="100";
+    document.querySelector(".video") .style.filter='brightness(10%)';
     video.pause(); 
 }) 
