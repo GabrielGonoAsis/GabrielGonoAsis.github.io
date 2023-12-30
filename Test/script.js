51d97d0325bdd992381b4bffa1c17c600509b68b
GabrielAsis <153348909+GabrielGonoAsis@users.noreply.github.com>
2023-12-30 15:10:12 +0400

    GSAP Animations

--- Test/script.js
+++ Test/script.js
@@ -1,26 +1,42 @@
-/*=============== SWIPER JS ===============*/
-let swiperCards = new Swiper(".card__content", {
-  loop: true,
-  spaceBetween: 32,
-  grabCursor: true,
+const container = document.querySelector(".scroll_container");
+const sections = gsap.utils.toArray(".section");
 
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-    dynamicBullets: true,
-  },
-
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-
-  breakpoints:{
-    600: {
-      slidesPerView: 2,
-    },
-    968: {
-      slidesPerView: 3,
-    },
-  },
+let scrollTween = gsap.to(sections, {
+  xPercent: -142 * (sections.length - 1),
+  ease: "none",
+  scrollTrigger: {
+    trigger: container,
+    pin: true,
+    scrub: 1,
+    end: "+=2000",
+    markers: false,
+  }
 });
+
+function animateText(textSelector, yValue, xValue, opacityValue, durationValue, easeType, staggerValue) {
+  sections.forEach((section) => {
+    let text = section.querySelectorAll(textSelector);
+    
+    if (text.length === 0) return;
+    
+    gsap.from(text, {
+      y: yValue,
+      x: xValue,
+      opacity: opacityValue,
+      duration: durationValue,
+      ease: easeType,
+      stagger: staggerValue,
+      scrollTrigger: {
+        trigger: section,
+        containerAnimation: scrollTween,
+        start: "left 85%",
+        markers: false,
+      },
+    });
+  });
+}
+
+animateText(".anim", -130, 0, 0, 1, "power3.inOut", 0.1);
+animateText(".anim2", 130, 0, 0, 1, "power3.inOut", 0.1);
+animateText(".anim3", 0, 400, 0, 1.2, "power2.inOut", 0);
+animateText(".anim4", 0, -400, 0, 1.2, "power2.inOut", 0);
--- script.js
+++ script.js
@@ -16,93 +16,7 @@ hover_text.addEventListener("mouseleave", function (e) {
     video.pause(); 
 }) 
 
-
-//horizontal scrolling
-const stickySections = [...document.querySelectorAll('.sticky_wrap')]
-
-window.addEventListener('scroll', (e) => {
-  for(let i = 0; i < stickySections.length; i++){
-    transform(stickySections[i])
-  }
-})
-
-function transform(section) {
-
-  const offsetTop = section.parentElement.offsetTop;
-
-  const scrollSection = section.querySelector('.horizontal_scroll')
-
-  let percentage = ((window.scrollY - offsetTop) / window.innerHeight) * 100;
-
-  percentage = percentage < 0 ? 0 : percentage > 400 ? 400 : percentage;
-
-  scrollSection.style.transform = `translate3d(${-(percentage)}vw, 0, 0)`
-}
-
-//removes navigation bar when in key-features section
-document.addEventListener("DOMContentLoaded", function () {
-  const keyFeaturesSection = document.getElementById('key-features');
-  const header = document.getElementById('header');
-
-  function isOverKeyFeatures() {
-      const keyFeaturesTop = keyFeaturesSection.offsetTop;
-      const keyFeaturesBottom = keyFeaturesTop + keyFeaturesSection.offsetHeight;
-      const scrollY = window.scrollY;
-      return scrollY >= keyFeaturesTop && scrollY <= keyFeaturesBottom;
-  }
-
-  function handleScroll() {
-      if (isOverKeyFeatures()) {
-          document.getElementById("header").style.opacity = '0';
-      } else {
-          document.getElementById("header").style.opacity = '100';
-      }
-  }
-
-  window.addEventListener('scroll', handleScroll);
-
-  handleScroll();
-
-  var prevScrollpos = window.scrollY;
-  window.onscroll = function() {
-  var currentScrollPos = window.scrollY;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById('header').style.top = "0";
-  } else {
-    document.getElementById('header').style.top = "-60px";
-  }
-    prevScrollpos = currentScrollPos;
-  }
-});
-
-window.addEventListener('load', videoScroll);
-window.addEventListener('scroll', videoScroll);
-
-//autoplay on scroll for key-features videos
-function videoScroll() {
-
-  if ( document.querySelectorAll('.scroll_contents video').length > 0) {
-    var windowWidth = window.innerWidth,
-        videoEl = document.querySelectorAll('.scroll_contents video');
-
-    for (var i = 0; i < videoEl.length; i++) {
-
-      var thisVideoEl = videoEl[i],
-          videoWidth = thisVideoEl.clientWidth,
-          videoClientRect = thisVideoEl.getBoundingClientRect().left;
-
-      if ( videoClientRect <= ( (windowWidth) - (videoWidth*.5) ) && videoClientRect >= ( 0 - ( videoWidth*.5 ) ) ) {
-        thisVideoEl.play();
-      } else {
-        thisVideoEl.pause();
-      }
-
-    }
-  }
-
-}
-
-//carousels for characters
+//carousels for characters using swiper
 
 //friends carousels
 let swiperCards = new Swiper(".friends .card__content", {
@@ -171,7 +85,7 @@ let swiperCards2 = new Swiper(".foes .card__content", {
 });
 
 
-//popup for markers in map using gsap animations
+//popup for markers in map using GSAP animations
 
 //shows popup
 function showPopup(title, content, imagePath) {
@@ -241,17 +155,236 @@ function hideImages() {
 }
 
 
+//GSAP animations
+gsap.registerPlugin(ScrollTrigger)
+
+//slogan animation
+gsap.fromTo('.first-word', 
+{opacity: 0, 
+  y:-300
+}, 
+{opacity: 1, 
+  y: 0, 
+  duration: 2, 
+  ease: "power2.inOut"  , 
+  delay: 0 
+});
+
+gsap.fromTo('.second-word', 
+{opacity: 0, 
+  y:-300
+}, 
+{opacity: 1, 
+  y: 0, 
+  duration: 2, 
+  ease: "power2.inOut", 
+delay: 2 
+});
+
+//trailer video animation
+let tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".trailer",
+    start: "20% 80%",
+    end: "80% 50px",
+    scrub: 2,
+    markers: false,
+    toggleActions: "play none none none",
+  }
+});
+
+tl.to(".trailer",
+{opacity: 1, 
+  scale: 1,
+  duration: 1, 
+  ease: "power2.inOut", 
+  delay: 0
+})
+.to(".trailer",
+{opacity: 0, 
+  scale: 0.8,
+  duration: 1, 
+  ease: "power2.inOut", 
+  delay: 0
+});
+
+//overview section animation
+gsap.fromTo(".header-overview", //header
+{opacity: 0, 
+  x: -400,
+},
+{ scrollTrigger: {
+  trigger: ".overview",
+  start: "30% 80%",
+  end: "90% center",
+  markers: false,
+  toggleActions: "play none none none",
+},
+  opacity: 1, 
+  x: 0,
+  duration: 1, 
+  ease: "power3.inOut", 
+  delay: 0}
+);
+
+gsap.fromTo(".overview-hr", //horizontal line
+{opacity: 0, 
+  x: -400,
+},
+{ scrollTrigger: {
+  trigger: ".overview",
+  start: "30% 80%",
+  end: "90% center",
+  markers: false,
+  toggleActions: "play none none none",
+},
+  opacity: 1, 
+  x: 0,
+  duration: 1, 
+  ease: "power3.inOut", 
+  delay: 0.3}
+);
+
+gsap.fromTo(".p-overview1", //paragraph
+{opacity: 0, 
+  y: -50,
+},
+{ scrollTrigger: {
+  trigger: ".overview",
+  start: "30% 80%",
+  end: "90% center",
+  markers: false,
+  toggleActions: "play none none none",
+},
+  opacity: 1, 
+  y: 0,
+  duration: 1, 
+  ease: "power3.inOut", 
+  delay: 0}
+);
+
+gsap.fromTo(".p-overview2", //paragraph
+{opacity: 0, 
+  y: -50,
+},
+{ scrollTrigger: {
+  trigger: ".overview",
+  start: "30% 80%",
+  end: "90% center",
+  markers: false,
+  toggleActions: "play none none none",
+},
+  opacity: 1, 
+  y: 0,
+  duration: 1, 
+  ease: "power3.inOut", 
+  delay: 0.3}
+);
+
+//key features section
+const container = document.querySelector(".scroll_container");
+const sections = gsap.utils.toArray(".section");
+
+let scrollTween = gsap.to(sections, {
+  xPercent: -142 * (sections.length - 1),
+  ease: "none",
+  scrollTrigger: {
+    trigger: container,
+    pin: true,
+    scrub: 1,
+    end: "+=3000",
+    markers: false,
+  }
+});
+
+function animateText(textSelector, yValue, xValue, opacityValue, durationValue, easeType, staggerValue) {
+  sections.forEach((section) => {
+    let text = section.querySelectorAll(textSelector);
+    
+    if (text.length === 0) return;
+    
+    gsap.from(text, {
+      y: yValue,
+      x: xValue,
+      opacity: opacityValue,
+      duration: durationValue,
+      ease: easeType,
+      stagger: staggerValue,
+      scrollTrigger: {
+        trigger: section,
+        containerAnimation: scrollTween,
+        start: "left 85%",
+        markers: false,
+      },
+    });
+  });
+}
+
+animateText(".anim", -130, 0, 0, 1, "power3.inOut", 0.1);
+animateText(".anim2", 130, 0, 0, 1, "power3.inOut", 0.1);
+animateText(".anim3", 0, -200, 0, 1.2, "power2.inOut", 0);
+animateText(".anim4", 0, 200, 0, 1.2, "power2.inOut", 0);
 
 
+//autoplay on scroll for key-features videos
+function videoScroll() {
 
+  if ( document.querySelectorAll('.features-vid video').length > 0) {
+    var windowWidth = window.innerWidth,
+        videoEl = document.querySelectorAll('.features-vid video');
 
+    for (var i = 0; i < videoEl.length; i++) {
 
+      var thisVideoEl = videoEl[i],
+          videoWidth = thisVideoEl.clientWidth,
+          videoClientRect = thisVideoEl.getBoundingClientRect().left;
 
+      if ( videoClientRect <= ( (windowWidth) - (videoWidth*.9) ) && videoClientRect >= ( 0 - ( videoWidth*.3 ) ) ) {
+        thisVideoEl.play();
+      } else {
+        thisVideoEl.pause();
+      }
 
+    }
+  }
 
+}
 
+//removes navigation bar when in key-features section
+document.addEventListener("DOMContentLoaded", function () {
+  const keyFeaturesSection = document.getElementById('key-features');
+  const header = document.getElementById('header');
 
+  function isOverKeyFeatures() {
+      const keyFeaturesTop = keyFeaturesSection.offsetTop;
+      const keyFeaturesBottom = keyFeaturesTop + keyFeaturesSection.offsetHeight;
+      const scrollY = window.scrollY;
+      return scrollY >= keyFeaturesTop && scrollY <= keyFeaturesBottom;
+  }
 
+  function handleScroll() {
+      if (isOverKeyFeatures()) {
+          document.getElementById("header").style.opacity = '0';
+      } else {
+          document.getElementById("header").style.opacity = '100';
+      }
+  }
 
+  window.addEventListener('scroll', handleScroll);
 
+  handleScroll();
 
+  var prevScrollpos = window.scrollY;
+  window.onscroll = function() {
+  var currentScrollPos = window.scrollY;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById('header').style.top = "0";
+  } else {
+    document.getElementById('header').style.top = "-60px";
+  }
+    prevScrollpos = currentScrollPos;
+  }
+});
+
+window.addEventListener('load', videoScroll);
+window.addEventListener('scroll', videoScroll);
